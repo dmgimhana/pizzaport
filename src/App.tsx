3a7173088e7fa31e4d82470d99c6ce3bddd47b7c
dmgimhana <dmgimhana@gmail.com>
2025-06-19 15:36:38 +0100

    included some basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Cart from './features/cart/Cart'
+import Menu from './features/menu/Menu'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+import Home from './ui/Home'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/menu', element: <Menu /> },
+  { path: '/cart', element: <Cart /> },
+  { path: '/order/new', element: <CreateOrder /> },
+  { path: '/order/:orderId', element: <Order /> },
+])
+
 function App() {
-  return <div>PizzaPort</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
