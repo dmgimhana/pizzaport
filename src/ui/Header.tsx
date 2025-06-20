5aa26e7c6998ef17697cdd5ddebd1d2493954837
dmgimhana <dmgimhana@gmail.com>
2025-06-20 13:52:15 +0100

    created AppLayout, and Header

---
+++ src/ui/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">PizzaPort Co.</Link>
+      <p>PizzaPort</p>
+    </header>
+  )
+}
+
+export default Header
