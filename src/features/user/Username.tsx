740a8c2084328895f1c078e5ecb24e9b4dfecd9f
dmgimhana <dmgimhana@gmail.com>
2025-07-13 12:20:57 +0100

    modeled user state with redux

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,7 +1,14 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../../models'
+
 function Username() {
+  const username = useSelector((state: RootState) => state.user.username)
+
+  if (!username) return null
+
   return (
     <div className="hidden font-sans text-sm font-semibold md:block">
-      Gimhana
+      {username}
     </div>
   )
 }
